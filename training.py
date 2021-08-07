dfb009f575ca94addbf254b0d8c838a40eff935e
Colm <evansgcolm@gmail.com>
2021-08-07 10:50:40 -0500

    Removed unnecessary import.

--- training.py
+++ training.py
@@ -1,14 +1,13 @@
-import os
-from dotenv import load_dotenv
+from dotenv import  dotenv_values
 
 
 
 
 
-DATA_PATH = os.environ['BLOG_DATA_PATH']
+DATA_PATH = dotenv_values(".env")['BLOG_DATA_PATH']
 
 
-
+print(DATA_PATH)
 
 
 
